95338a4282bfeb5c7d3e91f49cf5c14d20ca0cfe
bigbenlund <outlxawed@gmail.com>
2026-09-17 12:49:22 -0400

    thermometer

--- csce242-spring26/exercises/04js/scripts/intro.js
+++ csce242-spring26/exercises/04js/scripts/intro.js
@@ -1,88 +1,107 @@
+//Shows a message when the button is clicked
 document.getElementById("btn-show-message").onclick = (e) => {
     document.getElementById("p-message").innerHTML = "Hello World";
-    e.target.innerHTML= "done!";
-}; //function is defined by its execution .. hence:  => {}
+    e.target.innerHTML = "done!";
+};
 
-document.getElementById("link1").onclick = (e) => {
-    e.preventDefault(); //dont go to links destination
+//styles the link when it's clicked
+document.getElementById("link").onclick = (e) => {
+    e.preventDefault(); //don't go to links destination
     e.target.classList.add("cool-link");
 };
 
-document.getElementById("ball-button").onclick = (e) => {
-    console.log("hi");
-    document.getElementById("ball").classList.add("bouncing-ball");
-};
+//when button clicked make ball bounce
+document.getElementById("btn-bounce").onclick = (e) => {
+    document.getElementById("ball").classList.toggle("bouncing-ball");
+}
 
+document.querySelector("#toggle-nav").onclick = () => {
+    console.log("hello");
+    document.querySelector("#main-nav ul").classList.toggle("hidden");
+}
+
+
+//when you change the number of days since you watered your plant
+//show a message and change the image
 document.getElementById("txt-num-days").onkeyup = (e) => {
     const numDays = parseInt(e.target.value);
     const pMessage = document.getElementById("p-plant-msg");
     const plantImage = document.getElementById("img-plant");
     plantImage.classList.remove("hidden");
+    
+    if(numDays <= 2){
+        pMessage.innerHTML = `Let your plant rest it's only been ${numDays} day(s).`;
+        plantImage.src="https://dummyimage.com/150x150/556B2F/fff&text=Happy+Plant";
+    } else if(numDays <= 5){
+        pMessage.innerHTML = `Time to water it's been ${numDays} days.`;
+        plantImage.src="https://dummyimage.com/150x150/BDB76B/fff&text=Thirsty";
+    } else if(numDays <= 7) {
+        pMessage.innerHTML = `Oh no your plant is wilting it's been ${numDays} days.`;
+    } else {
+        pMessage.innerHTML = "Your plant is a gooner";
+    }
+}
 
-    if (numDays == NaN ) {
-        pMessage.innerHTML = `no plant`;
-        plantImage.src="";
-    }
-    if (numDays <=2 ) {
-        //pMessage.innerHTML = "Let yo plant rest" + numDays + "bruh"
-        pMessage.innerHTML = `Let yo plant rest its been ${numDays} days.`;
-        plantImage.src="images/healthy.png";
-    } else if (numDays <= 5) {
-        pMessage.innerHTML = `Time to wata it's been ${numDays} days.`;
-        plantImage.src="images/thirsty.png"
-    }
-    else if (numDays <= 7) {
-        pMessage.innerHTML = `Oh no ur plant its wilting, its been ${numDays} days.`;
-        plantImage.src="images/wilting.png";
-    }
-    else {
-        pMessage.innerHTML = `ur plant is dead x_x, its been ${numDays} days.`;
-        plantImage.src="images/dead.png";
-    }
-};
+/* thermometer */
+const GOAL = 10000;
+const goal = document.getElementById("don-goal").innerHTML = GOAL;
+document.getElementById("btn-donation").onclick = () => {
+    const donation = parseInt(document.getElementById("txt-donation").value);
+    percent = donation / GOAL*100;
+    document.getElementById("p-donation-message").innerHTML = `You are ${percent.toFixed(1)}% to ur goal`;
+    console.log(donation);
+
+    document.querySelector(":root").style.setProperty("--donation", percent + "%");
+}
 
 //counting
 let countInterval;
 let count = 0;
-const pCount = document.getElementById("p-const"); //if u need to access an element outside of the function, delcare it outside bro. 
-const btnStart = document.getElementById("btn-Start");
-const btnPause = document.getElementById("btn-Pause");
-const btnStop = document.getElementById("btn-Stop");
+const pCount = document.getElementById("p-count");
+const btnStart = document.getElementById("btn-start");
+const btnPause = document.getElementById("btn-pause");
+const btnStop = document.getElementById("btn-stop");
 btnPause.disabled = true;
 btnStop.disabled = true;
-btnStart.disabled = false;
 
-
-
-btnStart.onclick = () => {
+btnStart.onclick = () =>
+{
     countInterval = setInterval(()=>{
         pCount.innerHTML = ++count;
-    },500); //every half a second (500ms) the function in there will be called. SetInterval has 2 parameters. 
-    console.log("Start-clicked");
+    },500);
+    btnStart.disabled = true;
     btnPause.disabled = false;
     btnStop.disabled = false;
 };
 
-btnPause.onclick = () => {
+btnPause.onclick = () =>
+{
     clearInterval(countInterval);
-    console.log("Pause-clicked");
+    btnStart.disabled = false;
     btnPause.disabled = true;
-    
+    btnStop.disabled = true;
 };
 
-btnStop.onclick = () => {
-    console.log("Stop-clicked");
+btnStop.onclick = () =>
+{
     count=0;
     pCount.innerHTML = "";
     clearInterval(countInterval);
+    btnStart.disabled = false;
     btnPause.disabled = true;
     btnStop.disabled = true;
 };
 
 //date display
 setInterval(()=>{
-    const pDisplay = document.getElementById("date-Display");
-    const today = new Date(); //todays date
+    const pDisplay = document.getElementById("date-display");
+    const today = new Date();
+    const month = today.getMonth()+1;
+    const day = today.getDate();
+    const year = today.getFullYear();
+    const hours = today.getHours();
+    const minutes = today.getMinutes();
     const seconds = today.getSeconds();
-    pDisplay.innerHTML = today,seconds;
+
+    pDisplay.innerHTML = `${hours}:${minutes}:${seconds} ${month}/${day}/${year}`;
 }, 1000);
